5c1069b59bd0c42eb4a657fd80c4b77c526b5a22
Martinitworld <89170112+Martinitworld@users.noreply.github.com>
2021-11-30 17:27:34 -0600

    Update martintodo.py

--- martintodo.py
+++ martintodo.py
@@ -1,2 +1,10 @@
-mtodolist = ['buy my christmas lists','need to clean my room,'get my grocery','say hi to Frank']
-print(len(mtodolist))
+mtodolist = ['buy my christmas lists','need to clean my room','get my grocery','say hi to Frank']
+
+print(mtodolist)
+#print(len(mtodolist))
+
+print('This is my to do for the day')
+
+print(f' {len(mtodolist)} thing(s) left to do')
+for i in mtodolist:
+  print(i)
