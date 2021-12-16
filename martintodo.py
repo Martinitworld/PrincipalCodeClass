86b5be5636a4ac0ca886d24cfb97029e2ccc573d
Martinitworld <89170112+Martinitworld@users.noreply.github.com>
2021-12-16 17:30:17 -0600

    Update martintodo.py

--- martintodo.py
+++ martintodo.py
@@ -8,3 +8,34 @@ print('This is my to do for the day')
 print(f' {len(mtodolist)} thing(s) left to do')
 for i in mtodolist:
   print(i)
+  
+  add = ""
+remove = ""
+def addremove():
+  print ("Would you like to add or remove an item?\n")
+ 
+
+def addlist():
+  add = input("What would you like to do in your to do list? \n")
+  todolist.append(f"{add}\n")
+
+def removelist():
+  remove = input("What would you like to remove from your to do list? \n")
+  todolist.remove(f"{remove}\n")
+
+
+
+
+
+def todolist():
+  print (f"To do list: \n")
+
+while todolist > 0:
+  todolist()
+  
+  addremove()
+  
+  answer = input()
+  
+  if answer == "add":
+    add()
